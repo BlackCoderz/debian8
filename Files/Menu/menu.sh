62e0d7713fc3100729c53ab431e3beed373d3799
radzvpn <40193115+radzvpn@users.noreply.github.com>
2019-07-23 08:03:37 +0800

    Update menu.sh

--- Files/Menu/menu.sh
+++ Files/Menu/menu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Script by : Hiratechi
+# Script by : RadzVPN
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  Hiratechi           "
-echo -e "\e[93m                  a.k.a reputation                "
+echo -e "\e[93m           AutoScriptVPS by  RadzVPN           "
+echo -e "\e[93m                  RADZ FAMILY                "
 echo -e "\e[0m                                                   "
 echo -e "\e[93m        COMMANDS [menu/accounts/options/server]            "
 echo -e "\e[0m                                                   "
